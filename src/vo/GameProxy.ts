55ad9e6257233affb448ccdb9b459781afa56e23
Ashot Avagyan <avagyanah@gmail.com>
2019-04-07 11:54:55 +0400

    minor

--- src/vo/GameProxy.ts
+++ src/vo/GameProxy.ts
@@ -1,9 +1,17 @@
 import { Proxy } from '../../mvcx/model/Proxy';
+import { Model } from '../../mvcx/model/Model';
+import { GameVO } from './GameVO';
 
-export class GameProxy<T> extends Proxy<T> {
+export class GameProxy extends Proxy<GameVO> {
   static NAME: string = `GameProxy`;
 
   constructor() {
     super();
+  }
+
+  public onRegister(model: Model): void {
+    super.onRegister(model);
+
+    this.vo = new GameVO();
   }
 }
---
+++ src/vo/GameVO.ts
@@ -0,0 +1,5 @@
+export class GameVO {
+  constructor() {
+    // ...
+  }
+}
